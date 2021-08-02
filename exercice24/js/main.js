68ab08ad81186d96c1c0d5f2acd3bd992f55574a
Francois Janson <jansonfrancois@gmail.com>
2021-08-02 15:08:11 +0200

    Exercises finished

--- exercice24/js/main.js
+++ exercice24/js/main.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
     $("button").click(function() {
-        // The id #bg is not working...
         $("#bg").animate({
             translate: "100px",
-
         })
     })
 })
