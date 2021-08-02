69a9e875bef5048c822edd055e738a0441f811c8
Francois Janson <jansonfrancois@gmail.com>
2021-08-02 14:47:47 +0200

    Exercises finished

--- exercice34/js/main.js
+++ exercice34/js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
     $("button").click(function() {
         let input = $("input").val();
-        console.log(input)
         $.ajax({
-
+            url: "https://restcountries.eu/rest/v2/name/" + input,
+            success: function(data, statuts, response) {
+                $("#country").html(data[0].name)
+                $("#capital").html(data[0].capital)
+            }
         })
     })
 })
